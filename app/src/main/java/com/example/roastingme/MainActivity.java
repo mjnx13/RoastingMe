34eb8f3a561ad0be4031246f567f8822f21b6e5f
Minjun Song <ysmj9031@gmail.com>
2026-09-18 10:29:18 +0900

    chore: initialize Android Project

---
+++ app/src/main/java/com/example/roastingme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.roastingme;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+    }
+}
