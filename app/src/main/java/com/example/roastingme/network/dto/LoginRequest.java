d00c04f603cd1b1a491ef7ce5c8e5e0f3d85dabd
Minjun Song <ysmj9031@gmail.com>
2026-09-21 10:59:28 +0900

    feat: 로그인 요청 DTO(LoginRequest) 추가

--- app/src/main/java/com/example/roastingme/network/dto/LoginRequest.java
+++ app/src/main/java/com/example/roastingme/network/dto/LoginRequest.java
@@ -1,4 +1,30 @@
 package com.example.roastingme.network.dto;
 
+import androidx.annotation.NonNull;
+import com.google.gson.annotations.SerializedName;
+
 public class LoginRequest {
+
+    @SerializedName("email")
+    @NonNull
+    private final String email;
+
+    @SerializedName("password")
+    @NonNull
+    private final String password;
+
+    public LoginRequest(@NonNull String email, @NonNull String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    @NonNull
+    public String getPassword() {
+        return password;
+    }
 }
