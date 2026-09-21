9f3765c727471638cf1732131dd1ba6c39bdb2bc
Minjun Song <ysmj9031@gmail.com>
2026-09-21 12:42:35 +0900

    feat: 내 정보 조회 API(UserApi) 인터페이스 추가

--- app/src/main/java/com/example/roastingme/network/UserApi.java
+++ app/src/main/java/com/example/roastingme/network/UserApi.java
@@ -1,4 +1,12 @@
 package com.example.roastingme.network;
 
-public class UserApi {
+import com.example.roastingme.network.dto.MeResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface UserApi {
+
+    @GET("api/users/me")
+    Call<MeResponse> getMe();
 }
