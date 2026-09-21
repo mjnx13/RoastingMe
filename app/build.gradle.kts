f3b5d953c21028ae27fc8404203fcc2dc8b42cbd
Minjun Song <ysmj9031@gmail.com>
2026-09-21 14:51:38 +0900

    build(deps): 네트워크 디버깅용 logging-interceptor 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
